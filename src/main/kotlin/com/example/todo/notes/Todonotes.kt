73214c4db3b90b6e5f01acf95c66d9e82ffcbdff
Joe Malebe <Joe.Malebe@standardbank.co.za>
2026-04-02 15:14:45 +0200

    zzzz

--- src/main/kotlin/com/example/todo/notes/Todonotes.kt
+++ src/main/kotlin/com/example/todo/notes/Todonotes.kt
@@ -2,22 +2,37 @@ package com.example.todo.notes
 
 import com.example.todo.notes.formats.kotlinXMessage
 import com.example.todo.notes.formats.kotlinXMessageLens
+import org.http4k.core.Body
 import org.http4k.core.HttpHandler
+import org.http4k.core.Method
 import org.http4k.core.Method.GET
 import org.http4k.core.Response
+import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.core.with
 import org.http4k.filter.DebuggingFilters.PrintRequest
+import org.http4k.lens.Lens
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
+val lens = Lens<Body,Todo>
+
 val app: HttpHandler = routes(
+    "/" bind GET to {
+        Response(OK)
+    },
     "/ping" bind GET to {
         Response(OK).body("pong")
     },
+
+    "/post" bind Method.POST to {
+        val resBody = it.body
+        Response(Status.NO_CONTENT)
+    },
+
 
     "/formats/json/kotlinx" bind GET to {
         Response(OK).with(kotlinXMessageLens of kotlinXMessage)
--- src/test/kotlin/com/example/todo/notes/TodonotesTest.kt
+++ src/test/kotlin/com/example/todo/notes/TodonotesTest.kt
@@ -1,8 +1,12 @@
 package com.example.todo.notes
 
+import com.example.todo.notes.app
+import org.http4k.core.Method
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
+import org.http4k.core.Status.Companion.NO_CONTENT
 import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -14,4 +18,23 @@ class TodonotesTest {
         assertEquals(Response(OK).body("pong"), app(Request(GET, "/ping")))
     }
 
+    @Test
+    fun `return ok when get default route`() {
+       val res =  app(Request(method = GET,"/"))
+        assertEquals(OK,res.status)
+    }
+
+    @Test
+    fun `when post todos is successful then return no content`() {
+        val res  =  app(Request(method = Method.POST,"/post"))
+
+        assertEquals(NO_CONTENT, res.status)
+    }
+
+    @Test
+    fun `when post with no todo object then return bad request`() {
+        val res = app(Request(method = Method.POST, "/post"))
+
+        assertEquals(BAD_REQUEST, res.status)
+    }
 }
